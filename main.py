2be43f2cb45a7185e06f995ebf505ff3a53ef078
Tyler Carbone <tcarbone073@gmail.com>
2021-08-15 11:26:49 +0000

    Intel script run successfully

--- main.py
+++ main.py
@@ -24,11 +24,11 @@ product_lines = {
 # 11th Generation Core i9, 7th Generation Core i7, 10th Generation Core i3).
 # Here, we get a list of all URLs, each one corresponding to a family of
 # processors.
-for key in product_lines.keys():
+for i, key in enumerate(product_lines.keys()):
     family_urls = scrape.get_family_links(ROOT_URL, product_lines[key])
 
     cpu_specs = {}
-    for i, family_url in enumerate(family_urls):
+    for j, family_url in enumerate(family_urls):
 
         # Here, we get a list of all of the URLs for each CPU model within each
         # product family. For example, within the product family '9th
@@ -36,7 +36,7 @@ for key in product_lines.keys():
         # i7-9750H', 'Core i7-9850HE', etc. 
         model_urls = scrape.get_model_links(family_url)
 
-        for j, model_url in enumerate(model_urls):
+        for k, model_url in enumerate(model_urls):
 
             # Return a name and dictionary to each CPU
             (cpu_data, name) = scrape.get_cpu_data(model_url)
@@ -46,16 +46,15 @@ for key in product_lines.keys():
             cpu_specs[name] = cpu_data
             
             # Print a status message
-            print("%s (Family %d/%d, Model %d/%d)" % 
-                (name, i+1, len(family_urls), j+1, len(model_urls)))
-            
+            print("%s (Product %d/%d, Family %d/%d, Model %d/%d)" % 
+                (name, i+1, len(product_lines), j+1, len(family_urls), k+1, len(model_urls)))
+
+
     # Send the output of the script to a JSON file named for the given product
     # family
     filename = f"./outfiles/{key}.json"
     with open(filename, "w") as outfile:
         json.dump(cpu_specs, outfile, indent=4)
-
-    break
 
 print("Finished in %d seconds." % (time.time() - start_time))
 
